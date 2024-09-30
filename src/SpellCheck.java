57564c33b083e225a80be54db8e524d068a19a13
kpi <kieran.pichai@menloschool.org>
2024-09-29 21:40:31 -0700

    asdasd asd asd

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * Spell Check
  * A puzzle written by Zach Blick
@@ -8,8 +12,6 @@
  * */
 
 public class SpellCheck {
-
-
     /**
      * checkWords finds all words in text that are not present in dictionary
      *
@@ -17,26 +19,31 @@ public class SpellCheck {
      * @param dictionary The list of all accepted words.
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
-    public String[] generateHashmap(String[] dictionary) {
-            String[] test = new String[1000000000];
-            for (int i = 0; i < 100; i++) {
-                test[(int)hashString(dictionary[i])] = dictionary[i];
-            }
-
-            return test;
+    public Boolean[] generateHashmap(String[] dictionary) {
+        Boolean[] test = new Boolean[1000000000];
+        for (String s : dictionary) {
+            test[(int) hashString(s)] = true;
+        }
+        return test;
     }
 
     public long hashString(String stringToHash) {
-        long hash = 7;
+        long hash = 5;
         for (int i = 0; i < stringToHash.length(); i++) {
-            hash = hash * 31 + stringToHash.charAt(i);
+            hash = hash * 7 + (stringToHash.charAt(i) - 64);
         }
-        System.out.println(hash);
         return hash;
     }
 
     public String[] checkWords(String[] text, String[] dictionary) {
-        String[] test = generateHashmap(dictionary);
-        return test;
+        Boolean[] test = generateHashmap(dictionary);
+        ArrayList<String> incorrect = new ArrayList<String>();
+        for (String word : text) {
+            if (!test[(int) hashString(word)]) {
+                incorrect.add(word);
+            }
+        }
+        String[] wrong = new String[incorrect.size()];
+        return incorrect.toArray(wrong);
     }
 }
